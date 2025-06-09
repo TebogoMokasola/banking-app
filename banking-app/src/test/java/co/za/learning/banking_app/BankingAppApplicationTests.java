018aba03790a314f07ed5c9c4d0a9636cc68abb2
QXZ498G <tebogo.mokasola@bmwithub.co.za>
2025-06-09 20:12:57 +0200

    initial commit

---
+++ banking-app/src/test/java/co/za/learning/banking_app/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.learning.banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
