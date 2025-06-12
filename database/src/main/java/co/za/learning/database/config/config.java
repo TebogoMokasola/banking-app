7009ba1344c1098de818672083a86a797198b246
GomolemoNdlovu <98579925+GomolemoNdlovu@users.noreply.github.com>
2025-06-12 13:16:52 +0200

    Update config.java
    Code Clean-Up

--- database/src/main/java/co/za/learning/database/config/config.java
+++ database/src/main/java/co/za/learning/database/config/config.java
@@ -13,11 +13,8 @@ public class config {
 
     @Bean(destroyMethod = "")
     public DataSource dataSource() throws SQLException, UnknownHostException, SocketException {
-        // Set H2 server properties to bind to all network interfaces
         System.setProperty("h2.bindAddress", "0.0.0.0");
         String userDir = System.getProperty("user.dir");
-
-        // Start H2 TCP server
         String[] args = {
                 "-tcpPort", "8082",
                 "-tcpAllowOthers",
@@ -25,11 +22,7 @@ public class config {
                 "-baseDir", userDir.concat("/h2db")
         };
         Server.createTcpServer(args).start();
-
-        // Dynamically retrieve the host IP address
         String hostIp = IpAddressUtils.getHostIp();
-
-        // Configure DataSource with host IP
         return DataSourceBuilder.create()
                 .driverClassName("org.h2.Driver")
                 .url("jdbc:h2:tcp://" + hostIp + ":8082/./h2db/database")
