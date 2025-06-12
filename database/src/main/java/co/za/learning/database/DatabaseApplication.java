890c33590c6c8c273fef70c4359e4fc1f7d2e900
GomolemoNdlovu <98579925+GomolemoNdlovu@users.noreply.github.com>
2025-06-12 13:17:31 +0200

    Update DatabaseApplication.java
    Clean-Up

--- database/src/main/java/co/za/learning/database/DatabaseApplication.java
+++ database/src/main/java/co/za/learning/database/DatabaseApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DatabaseApplication {
 
     public static void main(String[] args) {
-        System.out.println("Starting DATABASE MICROSERVICE...");
-
-        // Start Spring Boot application
         SpringApplication.run(DatabaseApplication.class, args);
-
-        System.out.println("DATABASE MICROSERVICE STARTED...");
     }
 }
