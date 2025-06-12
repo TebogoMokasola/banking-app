a607c16c7bde155803f3a0870822b1c17404393e
GomolemoNdlovu <98579925+GomolemoNdlovu@users.noreply.github.com>
2025-06-12 13:18:16 +0200

    Update IpAddressUtils.java
    Code Clean-Up

--- database/src/main/java/co/za/learning/database/config/IpAddressUtils.java
+++ database/src/main/java/co/za/learning/database/config/IpAddressUtils.java
@@ -1,24 +1,18 @@
 package co.za.learning.database.config;
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
 public class IpAddressUtils {
-
     public static String getHostIp() throws SocketException {
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-
         while (networkInterfaces.hasMoreElements()) {
             NetworkInterface networkInterface = networkInterfaces.nextElement();
-
             if (networkInterface.isLoopback() || !networkInterface.isUp()) {
                 continue;
             }
-
             Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-
             while (inetAddresses.hasMoreElements()) {
                 InetAddress inetAddress = inetAddresses.nextElement();
                 String ip = inetAddress.getHostAddress();
@@ -27,7 +21,6 @@ public class IpAddressUtils {
                 }
             }
         }
-
         throw new IllegalStateException("No IP address starting with 172.16 or 172.32 found");
     }
 }
